1aae56eddd8f24b9080fe522545781a928eb6762
Steve Kemp <steve@steve.org.uk>
2013-09-10 21:29:42 +0100

    TODO: We should cache failing IPs.

--- plugins/80-sfs.js
+++ plugins/80-sfs.js
@@ -4,7 +4,7 @@ exports.author  = function() { return "Steve Kemp <steve@steve.org.uk>" };
 
 
 //
-//  Test the IP the comment came-from.
+//  Test the IP the comment came-from against the stopforumspam site.
 //
 exports.testJSON = function ( obj, spam, ok, next )
 {
@@ -13,7 +13,7 @@ exports.testJSON = function ( obj, spam, ok, next )
     var done   = false;
 
     //
-    //  The URL request we're going to make
+    //  The URL request we're going to make.
     //
     var options = {
         host: 'www.stopforumspam.com',
@@ -22,7 +22,7 @@ exports.testJSON = function ( obj, spam, ok, next )
     };
 
     //
-    // A GET request
+    // A GET request.
     //
     var re = http.request(options, function(res) {
         var str = '';
@@ -30,9 +30,15 @@ exports.testJSON = function ( obj, spam, ok, next )
         res.on('data', function(chunk) {
             str += chunk;
         });
+
         res.on('end', function() {
             if ( str.indexOf( "<appears>yes</appears>" ) >= 0 )
             {
+                //
+                //  TODO: We should cache the IPs we've found
+                // listed for at least 48 hours, as we do on our
+                // legacy-site.
+                //
                 result = "SPAM: Listed in StopForumSpam.com" ;
                 spam( result );
             }
