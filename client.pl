5d34a25d871ed78ba8208a1ba659d20b135c3a1c
Steve Kemp <steve@steve.org.uk>
2013-09-10 21:11:33 +0100

    Updated to send options and have comments.

--- client.pl
+++ client.pl
@@ -9,21 +9,37 @@ use warnings;
 
 my $uri = 'http://localhost:8888/';
 
+#
+#  The data we'll send to the testing service.
+#
 my %data;
-$data{'name'} = 'Steve Kemp';
-#$data{'email'} = 'steve@example.com';
-$data{'ip'} = '109.194.111.184';
+$data{'name'}    = 'Steve Kemp';
+$data{'email'}   = 'steve@moi.com';
+$data{'options'} = "whitelist=127.0.0.1,mandatory=subject,mandatory=comment";
+$data{'ip'}      = '109.194.111.144';
+$data{'comment'} = '<p>This is my body ..</p>';
+$data{'subject'} = "This is the comment subject";
 
-$data{'body'} = '<p>This is my body ..</p>';
+#
+#  Encode it.
+#
 my $json = encode_json \%data;
 
 
+#
+#  We're going to use a HTTP POST.
+#
 my $req = HTTP::Request->new( 'POST', $uri );
 $req->header( 'Content-Type' => 'application/json' );
 $req->content( $json );
 
-
+#
+#  Send the request.
+#
 my $lwp = LWP::UserAgent->new;
 my $response = $lwp->request( $req );
 
+#
+#  Show the result
+#
 print $response->code . " " . $response->decoded_content . "\n";
