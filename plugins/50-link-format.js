45d2d745b8856a3507bfe0db69d845f68be41300
Steve Kemp <steve@steve.org.uk>
2013-09-11 09:09:56 +0100

    Test for multiple link-types.
      This matches the lagacy API:
            http://search.cpan.org/dist/Blog-Spam/lib/Blog/Spam/Plugin/multilinks.pm

---
+++ plugins/50-link-format.js
@@ -0,0 +1,39 @@
+exports.name    = function() {return "50-link-format.js" ; };
+exports.purpose = function() {return "Look for different linking stratergies" ; };
+exports.author  = function() { return "Steve Kemp <steve@steve.org.uk>" };
+
+
+//
+//
+//
+exports.testJSON = function ( obj, spam, ok, next)
+{
+    var options = obj['options'] || '';
+    var comment = obj['comment'] || '';
+
+    //
+    //  Regexps to find links of different forms
+    //
+    //
+    //
+    //  So we have a max-links count.  We need to count the links
+    //
+    var ahref = comment.match(/href=['"]https?:\/\//gi);
+    var url   = comment.match(/url=['"]?https?:\/\//gi);
+    var link  = comment.match(/link=['"]?https?:\/\//gi);
+
+    if ( ( ahref != null && ahref.length > 0 ) &&
+         ( url != null &&  url.length > 0 ) &&
+         ( link != null && link.length > 0 ) )
+    {
+        spam( "Multiple linking stratergies" );
+    }
+
+
+    //
+    //  We passed this plugin.
+    //
+    next("next");
+};
+
+
