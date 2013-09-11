878793d8c10087ad00fd96e9c87e2fabc9f551a6
Steve Kemp <steve@steve.org.uk>
2013-09-11 12:24:08 +0100

    Added plugin to validate MX for emails.

---
+++ plugins/15-requiremx.js
@@ -0,0 +1,49 @@
+//
+// Stock methods.
+//
+exports.name    = function() {return "15-requiremx.js" ; };
+exports.purpose = function() {return "Ensure submitted email addresses have an MX" ; };
+exports.author  = function() { return "Steve Kemp <steve@steve.org.uk>" };
+
+
+//
+//  Find the MX  for the user's email.
+//
+exports.testJSON = function ( obj, spam, ok, next )
+{
+    var mail   = obj['email']   || ""
+    var regexp = /^([^@]+)@(.*)$/
+
+    var match = regexp.exec( mail );
+    if ( match )
+    {
+        var domain = match[2].trim();
+
+        console.log( "Looking for MX for domain " + domain );
+
+        dns.resolveMx(domain, function (err, addresses) {
+            if (err)
+            {
+                spam( "No MX record for domain " + domain );
+            }
+            else
+            {
+                next( "next" );
+            }
+        });
+    }
+    else
+    {
+        next( "next" );
+    }
+
+};
+
+
+//
+// Init method..
+//
+exports.init = function (  )
+{
+    dns = require('dns');
+}
