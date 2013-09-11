d132c89a4ad191b9906e1b00c662d02ab26440e0
Steve Kemp <steve@steve.org.uk>
2013-09-11 21:06:00 +0100

    Added new plugin to count words, as per legacy API.
      This is a port of:
    
            http://search.cpan.org/dist/Blog-Spam/lib/Blog/Spam/Plugin/wordcount.pm

---
+++ plugins/45-wordcount.js
@@ -0,0 +1,70 @@
+exports.name    = function() {return "45-wordcount.js" ; };
+exports.purpose = function() {return "Count the words in the body and abort on too few." ; };
+exports.author  = function() { return "Steve Kemp <steve@steve.org.uk>" };
+
+
+//
+//  As per the legacy API we look look for word-count limits.
+//
+exports.testJSON = function ( obj, spam, ok, next)
+{
+    var options = obj['options'] || 'min-words=5';
+    var comment = obj['comment'] || '';
+
+    //
+    //  Count the words in the comment.
+    //
+    comment = comment.replace(/(^\s*)|(\s*$)/gi,"");
+    comment = comment.replace(/[ ]{2,}/gi," ");
+    comment = comment.replace(/\n /,"\n");
+    var wcount = comment.split(' ').length;
+
+    //
+    // Split the options.
+    //
+    var array = options.split( "," );
+
+    //
+    //  Look for "min-words=XX" and "max-words=XX" in the options.
+    //
+    var reg = /^(min-words|max-words)=(.*)$/;
+
+    //
+    //  Test each one
+    //
+    for (var i = 0; i < array.length; i++)
+    {
+        var option = array[i].trim()
+
+        //
+        //  If we match then we can compare the size.
+        //
+        var match = reg.exec( option );
+        if ( match )
+        {
+            var type = match[1].trim()
+            var size = match[2].trim()
+
+            if ( ( type == "min-words" ) &&
+                 ( wcount < size ) )
+            {
+                spam( "The comment had too few words." );
+                return
+            }
+
+            if ( ( type == "max-words" ) &&
+                 ( wcount > size ) )
+            {
+                spam( "The comment had too many words." );
+                return
+            }
+        }
+    }
+
+    //
+    //  We passed this plugin.
+    //
+    next("next");
+};
+
+
