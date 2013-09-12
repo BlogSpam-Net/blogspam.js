ec74bcdeba2b903022ae054f897febd9c74ce842
Steve Kemp <steve@steve.org.uk>
2013-09-12 09:43:45 +0100

    Added.  Incomplete.
      This plugin should work, but the remote site is currently
     showing a MySQL error so it is incomplete because I cannot
     test the return code.
    
      Annoyingly the XML::RPC code works, but I don't want to use
     that with node.js.

---
+++ plugins/85-linksleeve.js
@@ -0,0 +1,89 @@
+exports.name    = function() {return "85-linksleeve.js" ; };
+exports.purpose = function() {return "Look for blacklisted links via linksleeve.org." ; };
+exports.author  = function() { return "Steve Kemp <steve@steve.org.uk>" };
+
+
+//
+//  Test the IP the comment was submitted from against the linksleeve.org site.
+//
+exports.testJSON = function ( obj, spam, ok, next )
+{
+    var redis   = obj['_redis']
+    var ip      = obj['ip']      || '';
+    var comment = obj['comment'] || '';
+    var link    = obj['link']    || '';
+
+    //
+    //  Site seems to be having MySQL issues.
+    //
+    //  Disabled until it comes back.
+    //
+    next("next");
+
+    //
+    // Add the link to the comment body, if we got one.
+    //
+    if ( link.length > 0 )
+    {
+        comment += link;
+    }
+
+    //
+    // The data we're going to POST.
+    //
+    var post_data = querystring.stringify({
+        'content' : comment
+    });
+
+    //
+    //  The URL request we're going to make.
+    //
+    var options = {
+        host: 'www.linksleeve.org',
+        port: 80,
+        path: '/pslv.php',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': post_data.length
+        }
+    };
+
+    var post_req = http.request(options, function(res) {
+        res.setEncoding('utf8');
+
+        var str = '';
+
+        res.on('data', function(chunk) {
+            str += chunk;
+            console.log('Response: ' + chunk);
+        });
+
+        res.on('error', function(e) {
+            console.log( "Error sending POST request: " + e);
+            next("next");
+        });
+        res.on('end', function() {
+
+            // TODO: Test the result here.
+            next("next");
+        });
+    });
+
+    //
+    // Send the post.
+    //
+    post_req.write(post_data);
+    post_req.end();
+
+};
+
+
+//
+// Init method..
+//
+exports.init = function (  )
+{
+    http        = require('http');
+    querystring = require('querystring');
+}
