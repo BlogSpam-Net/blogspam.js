14ae19ac155ecc1a28d1574fed35a0c700115f6c
Steve Kemp <steve@steve.org.uk>
2013-09-14 12:23:59 +0100

    Use the external cidr_match library.
      This simplifies the logic considerably.

--- plugins/20-ip.js
+++ plugins/20-ip.js
@@ -23,6 +23,12 @@ exports.testJSON = function ( obj, spam, ok, next )
     var options = obj['options'] || ""
 
     //
+    // Handle to the cidr_match.js library.
+    //
+    var cidr    = obj['_cidr'];
+
+
+    //
     //  If we're missing an IP this is an error
     //
     if ( ! ip )
@@ -55,11 +61,6 @@ exports.testJSON = function ( obj, spam, ok, next )
     var m    = /^(whitelist|blacklist)=(.*)$/;
 
     //
-    //  The whitelist/blacklist can contain a CIDR range.
-    //
-    var cidr = /^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)\/([0-9]+)$/;
-
-    //
     //  For each option.
     //
     for (var i = 0; i < array.length; i++)
@@ -77,108 +78,31 @@ exports.testJSON = function ( obj, spam, ok, next )
             //
             var type  = match[1].trim()
             var ipval = match[2].trim()
+            var match = false;
 
             //
             // Is the IP value a CIDR range?
             //
-            var cidr_match = cidr.exec( ipval );
-            if ( cidr_match )
+            if ( ipval.indexOf( "/" ) >= 0 )
             {
-                //
-                // We work out how many wildcard bits there are.
-                //
-                // And how many IPs that range matches.  The biggest
-                // range we care about is a /24, because we're naive.
-                //
-                var slash = cidr_match[5];
-                var count = 256;
-
-                switch(slash){
-                    //
-                    // People can/will/do submit 192.168.0.0/255
-                    // because they don't understand how CIDR works.
-                    //
-                case "255":
-                    count = 256; break;
-
-                case "24":
-                    count = 256; break;
-                case "25":
-                    count = 128; break;
-                case "26":
-                    count = 64; break;
-                case "27":
-                    count = 32; break;
-                case "28":
-                    count = 16; break;
-                case "29":
-                    count = 8; break;
-                case "30":
-                    count = 4; break;
-                case "31":
-                    count = 2; break;
-                case "32":
-                    count = 1; break;
-                default:
-                    console.log( "CIDR - failed to match  " + ipval );
-                    count = 0; break;
-                };
-
-                //
-                // We setup a loop to iterate.
-                //
-                var match = false;
-                for ( var i = 0; i < count ; i++ )
-                {
-                    //
-                    // If we've not already matched ..
-                    //
-                    if ( ! match ) {
-
-                        //
-                        //  Build up the IP from 1.2.3.(4+i)
-                        //
-                        var tmp = cidr_match[1] + "." +
-                            cidr_match[2] + "." +
-                            cidr_match[3] + "." +
-                            ( parseInt(cidr_match[4],10) + parseInt(i,10) );
-
-                        //
-                        //  Does it match the submitters IP?
-                        //
-                        console.log( "CIDR-testing " + ip + " against " + tmp );
-                        if ( tmp == ip ) {
-                            console.log( "CIDR-matched!" );
-                            match = true ;
-                        }
-                    }
-                }
-
-                if ( match ) {
-                    if ( type == "whitelist" ) {
-                        ok( "Locally whitelisted IP, via options." );
-                        return;
-                    }
-                    if ( type == "blacklist" ) {
-                        spam( "Locally blacklisted IP, via options." );
-                        return;
-                    }
-                }
+                match = cidr.cidr_match( ip, ipval );
             }
             else
             {
                 //
-                //  Literal IP matching.
+                //  Otherwise literal comparison.
                 //
-                if ( ipval  == ip ) {
-                    if ( type == "whitelist" ) {
-                        ok( "Locally whitelisted IP, via options" );
-                        return;
-                    }
-                    if ( type == "blacklist" ) {
-                        spam( "Locally blacklisted IP, via options" );
-                        return;
-                    }
+                match = ( ipval ==  ip )
+            }
+
+            if ( match ) {
+                if ( type == "whitelist" ) {
+                    ok( "Locally whitelisted IP, via options." );
+                    return;
+                }
+                if ( type == "blacklist" ) {
+                    spam( "Locally blacklisted IP, via options." );
+                    return;
                 }
             }
         }
