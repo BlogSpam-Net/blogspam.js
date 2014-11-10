6cc648e5eded72c17fc30419200fc2fa528fcce4
Steve Kemp <steve@steve.org.uk>
2014-11-10 18:49:09 +0000

    Use the uvrun library to ensure DNS lookups complete.
    This updates #25.

--- plugins/70-httpbl.js
+++ plugins/70-httpbl.js
@@ -26,25 +26,51 @@ exports.testJSON = function ( obj, spam, ok, next )
         var key      = "keykeykeykey.";
         var lookup   = key + reversed + ".dnsbl.httpbl.org";
 
-        dns.resolve4(lookup, function (err, addresses) {
-            if (err)
-            {
-                next("next");
-            }
-            else
-            {
-                //
-                // Cache the result for two days.
-                //
-                redis.set( "blacklist-" + ip , "Listed in HTTP;bl" );
-                redis.expire( "blacklist-" + ip , 60*60*48 );
+        //
+        // Require the event-run-library
+        //
+        var uvrun = require( "uvrun" );
 
-                //
-                // Return the result.
-                //
-                spam( "Listed in HTTP;bl" );
-            }
+        //
+        // These are updated in the callback.
+        //
+        var e;
+        var a;
+
+        //
+        // Update the variables.
+        //
+        dns.resolve4(lookup, function (err, addresses) {
+             e = err;
+             a = addresses;
         });
+
+        //
+        // At this point we block/stall until the callback has
+        // finished.
+        //
+        while( ( !e ) && ( !a ) ) {
+            uvrun.runOnce();
+        }
+
+        if (e)
+        {
+            // error == No DNS result found.
+            next("next");
+        }
+        else
+        {
+            //
+            // Cache the result for two days.
+            //
+            redis.set( "blacklist-" + ip , "Listed in HTTP;bl" );
+            redis.expire( "blacklist-" + ip , 60*60*48 );
+
+            //
+            // Return the result.
+            //
+            spam( "Listed in HTTP;bl" );
+        }
     }
     else
     {
